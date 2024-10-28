58a4c1d358d3f0175850f355f7e65f9951f2798b
aortiz <aoa2eso@gmail.com>
2024-10-28 13:28:37 +0100

    Mejoras menores

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+"""Este script configura y ejecuta una aplicación FastAPI con carga de variables de entorno. 
+   Punto de entrada de la aplicación."""
+
 import logging
+from datetime import datetime
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from .routers import chat
 
 # Configuración del logger
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Cargamos las variables de entorno
+# Carga de las variables de entorno
 load_dotenv()
 
 app = FastAPI()
@@ -31,9 +34,14 @@ app.add_middleware(
 app.include_router(chat.router)
 
 
-@app.get("/")
-def read_root():
-    return {"Status": "API is running"}
+@app.get("/ping")
+def ping():
+    """
+    Endpoint para verificar el estado de la API.
+    Devuelve el estado y el tiempo actual.
+    """
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return {"Status": "API is running", "Time": current_time}
 
 
 if __name__ == "__main__":
