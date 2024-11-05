35d0fe260d5d6362d129d0609b9ccb69b2f3ebe9
aortiz <aoa2eso@gmail.com>
2024-11-05 15:53:56 +0100

    lógica de chat: actualizar modelos de datos,  gestión de historial y optimizar respuestas del LLM

--- routers/chat.py
+++ routers/chat.py
@@ -3,26 +3,24 @@
 import os
 import logging
 
-from typing import List
+from typing import Dict
 from datetime import datetime
 from fastapi import HTTPException, APIRouter, Query
 from fastapi.responses import StreamingResponse
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers.string import StrOutputParser
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory, InMemoryChatMessageHistory
 from langchain_openai import ChatOpenAI
 from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
+
 load_dotenv()
+
 logger = logging.getLogger(__name__)
 
 router = APIRouter()
-
-# TODO Plantear un modelo más eficiente.
-# global conversations
-
-# conversations: Dict[str, Dict]
-conversations = {}
-
 
 class Message(BaseModel):
     """Modelo para los mensajes de chat."""
@@ -31,11 +29,19 @@ class Message(BaseModel):
     timestamp: datetime = Field(datetime.now(), alias="timestamp")
 
 
-class Chat(BaseModel):
+class ChatRequest(BaseModel):
     """Modelo para los mensajes de chat."""
     conversation_id: str
-    messages: List[Message]
+    prompt: str
 
+
+class ChatResponse(BaseModel):
+    """Modelo para los mensajes de respuesta del LLM"""
+    conversation_id: str
+    response: str
+
+
+conversations: Dict[str, BaseChatMessageHistory] = {}
 
 llm = ChatOpenAI(
     model_name="gpt-4o-mini",
@@ -44,17 +50,19 @@ llm = ChatOpenAI(
 )
 
 
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    """Función para obtener el historial de mensajes de una conversación."""
+    if session_id not in conversations:
+        conversations[session_id] = InMemoryChatMessageHistory()
+    return conversations[session_id]
+
+
 def get_response_langchain(prompt: str, stream=False):
     """Función básica que obtiene una respuesta desde Langchain."""
     if not prompt:
-        prompt = "Dime un chiste sobre programación"
+        prompt = "Dime un como puedes ayudarme"
 
     system_prompt = (
-        # "You are an assistant for question-answering tasks. "
-        # "Use the following pieces of retrieved context to answer "
-        # "the question. If you don't know the answer, say that you "
-        # "don't know."
-        # "\n\n"
         '''
         Eres un asistente de inteligencia artificial para gestión documental y 
         redacción en la empresa Codexca. Ayuda a los usuarios principiantes a buscar 
@@ -68,19 +76,24 @@ def get_response_langchain(prompt: str, stream=False):
         innecesarios.
         Mantén un tono profesional y empático, promoviendo una 
         interacción fluida y accesible.
+        También responderás a todo tipo de preguntas aunque no estén relacionadas con el tema.
         '''
     )
     qa_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", system_prompt),
             ("human", f"{prompt}"),
+            MessagesPlaceholder(variable_name='history'),
         ]
     )
 
     logger.info("Obteniendo respuesta desde Langchain para el mensaje: %s",
                 qa_prompt.format_messages())
     try:
-        response = llm.invoke(qa_prompt.format_messages(), stream=stream)
+        chain = qa_prompt | llm
+        conversation_chain = RunnableWithMessageHistory(
+            chain, get_session_history, output_messages_key="response")
+        response = conversation_chain.invoke({'input': prompt}, stream=stream)
         # for chunk in response:
         #     yield text_chunk
         return response.content
@@ -90,9 +103,6 @@ def get_response_langchain(prompt: str, stream=False):
             status_code=500, detail="Error al procesar la solicitud.") from e
 
 
-def chat_langchain():
-    pass
-
 @router.post("/test_chat/")
 async def test_chat(message: Message, stream: bool = Query(False)):
     """Endpoint para probar la funcionalidad de chat."""
@@ -100,32 +110,54 @@ async def test_chat(message: Message, stream: bool = Query(False)):
         return StreamingResponse(get_response_langchain(prompt=message.content, stream=stream), media_type="text/plain")
     else:
         response = get_response_langchain(prompt=message.content)
-        return Message(role="system", content=response, timestamp=datetime.now())
+        return Message(role="ai", content=response, timestamp=datetime.now())
 
 
 @router.post("/chat/")
-async def chat_endpoint(chat_data: Chat):
-    """Endpoint para enviar mensajes de chat."""
-    conversation_id = chat_data.conversation_id
-    messages = chat_data.messages
+async def chat_history_endpoint(request: ChatRequest):
+    """Endpoint para interactuar con un modelo de lenguaje como un chatbot."""
 
-    if conversation_id not in conversations:
-        conversations[conversation_id] = {
-            "messages": [],
-            "last_update": datetime.now()
-        }
-    else:
-        conversations[conversation_id]["messages"].extend(messages)
-        conversations[conversation_id]["last_update"] = datetime.now()
+    conversation_id = request.conversation_id
+    user_input = request.prompt
+    system_prompt = (
+        '''
+        Eres un asistente de inteligencia artificial para gestión documental y 
+        redacción en la empresa Codexca. Ayuda a los usuarios principiantes a buscar 
+        información en documentos y redactar contenido nuevo de forma profesional, 
+        con un tono amable y accesible.
 
-    # Obtener la respuesta de Langchain
-    prompt = messages[-1].content if messages else ""
-    response_content = get_response_langchain(prompt)
+        Búsqueda de información: Encuentra y resume información clave 
+        en documentos según lo solicitado.
+        Redacción: Genera borradores claros y profesionales.
+        Soporte: Explica cada paso de forma sencilla y evita términos técnicos 
+        innecesarios.
+        Mantén un tono profesional y empático, promoviendo una 
+        interacción fluida y accesible.
+        También responderás a todo tipo de preguntas aunque no estén relacionadas con el tema.
+        '''
+    )
+    config = {'configurable': {'session_id': conversation_id}}
 
-    # Añadir la respuesta al historial de la conversación
-    conversations[conversation_id]["messages"].append(
-        Message(role="system", content=response_content)
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder('history'),
+        ]
     )
 
-    print("\nConversación completa: \n", conversations[conversation_id])
-    return {"response": response_content}
+    chain = qa_prompt | llm | StrOutputParser()
+
+    conversation_chain = RunnableWithMessageHistory(chain, get_session_history)
+
+    response = conversation_chain.invoke({'input': user_input}, config=config)
+    return ChatResponse(response=response, conversation_id=conversation_id)
+
+
+@router.get("/chat_trace/")
+def get_chat_trace(session_id: str):
+    """Función para obtener el historial de mensajes de una conversación."""
+    if session_id not in conversations:
+        conversations[session_id] = InMemoryChatMessageHistory()
+        logger.info(
+            "No se encontró historial de mensajes para la conversación %s. Creando una nueva.", session_id)
+    return conversations[session_id]
