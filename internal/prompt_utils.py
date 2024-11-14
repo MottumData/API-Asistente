0f3290e1d110a5fb31caebe79c83f19eea9d2c4b
aortiz <aoa2eso@gmail.com>
2024-11-14 17:56:28 +0100

    Mejorar la función load_prompt para requerir el nombre del prompt y simplificar la lectura del archivo JSON.

--- internal/prompt_utils.py
+++ internal/prompt_utils.py
@@ -9,24 +9,21 @@ logger = logging.getLogger(__name__)
 
 
 def load_prompt(file_path: str = SYSTEM_PROMPT_PATH, prompt_name: str = None):
-    """
-    Lee un prompt específico del sistema desde un archivo JSON.
-
-    :param file_path: Ruta del archivo JSON.
-    :param prompt_name: Nombre del prompt a cargar.
-    :return: El prompt solicitado o None si no se encuentra.
-    """
-    try:
-        with open(file_path, 'rb') as file:
-            prompts = json.loads(file.read())
-        logger.info("Prompt -> %s", prompt_name)
-        return prompts.get(prompt_name, None)
-    except FileNotFoundError:
-        logger.info("El archivo %s no se encontró.", file_path)
-        return None
-    except json.JSONDecodeError:
-        logger.info("Error al decodificar el archivo JSON %s.", file_path)
-        return None
+    """ Lee un prompt o esquema específico del sistema desde un archivo JSON."""
+    if prompt_name:
+        try:
+            with open(file_path, 'rb') as file:
+                prompts = json.loads(file.read())
+            logger.info("Prompt -> %s", prompt_name)
+            return prompts.get(prompt_name, None)
+        except FileNotFoundError:
+            logger.info("El archivo %s no se encontró.", file_path)
+            return None
+        except json.JSONDecodeError:
+            logger.info("Error al decodificar el archivo JSON %s.", file_path)
+            return None
+    else:
+        raise ValueError("El nombre del prompt es requerido.")
 
 
 def show_prompt(prompt_name: str = None):
