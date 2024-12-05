3e48caa364ffa147e19029ab353daaaa79489a50
aortiz <aoa2eso@gmail.com>
2024-12-05 09:48:26 +0100

    Actualizar la ruta del archivo de prompts del sistema y agregar un nuevo archivo de prompts V2 con mejoras en el contenido

--- internal/path_utils.py
+++ internal/path_utils.py
@@ -8,7 +8,7 @@ CHROMA_DB = "chroma_db"
 CHROMA_COLLECTION = "documents"
 STRUCTURE_DIRECTORY = '00_simulación_rag'
 STRUCTURE_FILE = RAG_DIR + '/' + 'structure.txt'
-SYSTEM_PROMPT_PATH = 'system_prompts.json'
+SYSTEM_PROMPT_PATH = 'system_prompts_V2.json'
 
 ALLOWED_EXTENSIONS = ['pdf', 'txt', 'json', '.pdf', '.json',
                       '.txt', 'csv', '.csv', 'xlsx', '.xlsx', 'xls', '.xls']
