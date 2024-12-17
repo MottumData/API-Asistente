ac53db4688a3c2684e7c70d91dc0a68691c3e316
aortiz <aoa2eso@gmail.com>
2024-12-17 11:04:55 +0100

    Agregar lógica para manejar la descarga de propuestas, guardar contenido y establecer títulos en tender_agent.py

--- routers/tender_agent.py
+++ routers/tender_agent.py
@@ -3,6 +3,7 @@
 import os
 import asyncio
 import logging
+import time
 from typing import List, Dict
 from pydantic import BaseModel
 
@@ -11,9 +12,14 @@ from langchain_core.prompts import PromptTemplate
 from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
 from openai import RateLimitError
 from fastapi import APIRouter, UploadFile, File, HTTPException
-from fastapi.responses import JSONResponse, StreamingResponse
+from fastapi.responses import JSONResponse, StreamingResponse, FileResponse
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_chroma import Chroma
+from docx import Document
+import markdown2
+from bs4 import BeautifulSoup
+import pypandoc
+
 
 from ..internal.path_utils import TEMP_DIR, CHROMA_DB, CHROMA_COLLECTION
 from ..internal.prompt_utils import load_prompt
@@ -82,6 +88,17 @@ class SaveContentRequest(BaseModel):
     content: Dict
 
 
+class DownloadProposalRequest(BaseModel):
+    """Modelo para la solicitud de descarga de la propuesta"""
+    proposal_id: str
+
+
+class SetTitleRequest(BaseModel):
+    """Modelo para la solicitud de guardado del contenido de la propuesta"""
+    proposal_id: str
+    title: str
+
+
 class TenderProposal(BaseModel):
     """Modelo para las propuestas de proyecto."""
     proposal_id: str
@@ -129,11 +146,11 @@ class TenderProposal(BaseModel):
     def set_index(self, index: List[str]):
         self.index = index
 
-    def get_key_ideas(self):
-        return self.key_ideas
+    # def get_key_ideas(self):
+    #     return self.key_ideas
 
-    def set_key_ideas(self, key_ideas: str):
-        self.key_ideas = key_ideas
+    # def set_key_ideas(self, key_ideas: str):
+    #     self.key_ideas = key_ideas
 
     def get_content(self):
         return self.content
@@ -143,6 +160,13 @@ class TenderProposal(BaseModel):
 
 
 proposals: Dict[str, TenderProposal] = {}
+
+
+def safe_check(doc: dict, key: str):
+    """ Verifica si una clave está presente en un diccionario. """
+    if key in doc.keys():
+        return True
+    return False
 
 
 def get_tender_data(doc):
@@ -202,12 +226,10 @@ async def upload_tdr(file: UploadFile = File(...), num_proposals: int = 5):
     return JSONResponse({"key points": tender_data, "complete summary": summary.content,
                          "related projects": related_project})
 
-# TODO - Que se devuelvan los proyectos unicos.
-
 
 @router.post('/related-projects/')
 def make_project_proposal(request: RelevantDocumentRequest, from_endpoint: bool = False):
-    """ Obtiene X propuestas de proyecto a partir de los términos de referencia
+    """ Obtiene hasta X propuestas de proyecto más afines a partir de los términos de referencia
         y la información extraída. """
 
     query = request.query
@@ -220,8 +242,18 @@ def make_project_proposal(request: RelevantDocumentRequest, from_endpoint: bool 
         search_type='mmr', search_kwargs={'k': num_proposals})
 
     related_docs = retriever.invoke(query)
-    related_docs = [{str(doc.metadata['source']).split(sep='\\')[-1]: doc.page_content}
-                    for doc in related_docs]
+
+    # Devuelve los documentos únicos
+    unique_docs = {}
+    for doc in related_docs:
+        source = str(doc.metadata['source']).split(sep='\\')[-1]
+        if source not in unique_docs:
+            unique_docs[source] = doc.page_content
+        else:
+            unique_docs[source] += doc.page_content
+
+    related_docs = [{source: content}
+                    for source, content in unique_docs.items()]
     if from_endpoint:
         return JSONResponse({"related_projects": related_docs, "query": query})
     return related_docs
@@ -239,11 +271,15 @@ def make_concept_notes(request: ConceptNotesRequest):
     tdr_summary = request.tdr_summary
 
     logger.info('Creating proposal with ID: %s', proposal_id)
-    
+
     proposal = TenderProposal(proposal_id=proposal_id)
     proposal.set_information_sources(information_sources)
     proposal.set_tdr_summary(tdr_summary)
     proposals[proposal_id] = proposal  # Crea una nueva propuesta
+
+    while not safe_check(proposals, proposal_id):
+        time.sleep(0.2)
+
     logger.info('Proposal created with ID: %s', proposal_id)
 
     logger.info('Generating concept notes for proposal: %s. Information sources: %s',
@@ -267,24 +303,27 @@ def make_concept_notes(request: ConceptNotesRequest):
     except RateLimitError as e:
         raise HTTPException(
             status_code=429,
-            detail="Documento demasiado extenso para el modelo. Pruebe con un documento más corto.") from e
+            detail="Documento demasiado extenso para el modelo. \
+                Pruebe con un documento más corto.") from e
     return JSONResponse({"concept_notes": response})
 
 
-@router.post('/save-concepts-notes/')
-def save_concept_notes(request: SaveConceptNotesRequest):
+@router.post('/save-concept-note/')
+async def save_concept_note(request: SaveConceptNotesRequest):
     """ Guarda las notas conceptuales de un proyecto a partir de su ID. """
 
     proposal_id = str(request.proposal_id)
     concept_notes = request.concept_notes
+
     proposal = proposals[proposal_id]
     proposal.set_concept_notes(concept_notes)
     proposals[proposal_id] = proposal
-    return JSONResponse(status_code=200, content={"message": "Notas conceptuales guardadas con éxito."})
+    return JSONResponse(status_code=200,
+                        content={"message": "Notas conceptuales guardadas con éxito."})
 
 
 @router.post('/make-index/')
-def make_index(request: IndexRequest):
+async def make_index(request: IndexRequest):
     """ Genera un índice de un proyecto. """
     proposal_id = str(request.proposal_id)
     proposal = proposals[proposal_id]
@@ -309,7 +348,7 @@ def make_index(request: IndexRequest):
 
 
 @router.post('/save-index/')
-def save_index(request: SaveIndexRequest):
+async def save_index(request: SaveIndexRequest):
     """ Guarda el índice de un proyecto a partir de su ID. """
     proposal_id = str(request.proposal_id)
     index = request.index
@@ -320,7 +359,7 @@ def save_index(request: SaveIndexRequest):
 
 
 @router.post('/make-content/')
-def make_proposal_content(request: ContentRequest):
+async def make_proposal_content(request: ContentRequest):
     """ Genera el contenido de una propuesta a partir de su ID. """
     proposal = proposals[str(request.proposal_id)]
     concept_notes = proposal.get_concept_notes()
@@ -330,29 +369,91 @@ def make_proposal_content(request: ContentRequest):
         'CODEXCA - DOSSIER DIGITAL 2021.pdf')
     content = {}
     json_template = load_prompt(prompt_name="project_writer")
-    for index, resume in index.items():
+    i = 1
+    for ind, resume in index.items():
         content_prompt = PromptTemplate(
             template=(json_template),
             input_variables=['index', 'description', 'concept_notes',
                              'business_information', 'tdr_summary'],
         )
         chain = content_prompt | llm
-        logger.info('Generating content for proposal: %s. Index: %s/%s',
-                    proposal.get_proposal_id(), index, len(index))
+        logger.info('Generating content for proposal: %s. Index name: %s - %s/%s',
+                    proposal.get_proposal_id(), ind, i, len(index))
         response = chain.invoke(
-            {"index": index, "description": resume, "concept_notes": concept_notes, "business_information": business_information, "tdr_summary": tdr_summary})
-        content[index] = response.content
+            {"index": ind, "description": resume, "concept_notes": concept_notes,
+             "business_information": business_information, "tdr_summary": tdr_summary})
+        content[ind] = response.content
+        i += 1
     return JSONResponse({"content": content})
 
 
+@router.post('/save-content/')
+async def save_proposal_content(request: SaveContentRequest):
+    """ Guarda el contenido de una propuesta a partir de su ID. """
+    proposal_id = str(request.proposal_id)
+    content = request.content
+    proposal = proposals[proposal_id]
+    proposal.set_content(content)
+    proposals[proposal_id] = proposal
+    return JSONResponse(status_code=200, content={"message": "Contenido guardado con éxito."})
+
+
 @router.get('/proposal-trace/')
-def get_proposal_trace(proposal_id: str):
+async def get_proposal_trace(proposal_id: str):
     """ Obtiene el registro de una propuesta a partir de su ID."""
     try:
         proposal = proposals[proposal_id]
     except KeyError:
         return JSONResponse(status_code=404, content={"message": "Código de propuesta no encontrado."})
     return JSONResponse(content=proposal.model_dump(mode='json'))
+
+
+@router.post('/set-title/')
+async def set_proposal_title(request: SetTitleRequest):
+    """ Guarda el título de una propuesta a partir de su ID. """
+    proposal_id = request.proposal_id
+    title = request.title
+    proposal = proposals[proposal_id]
+    proposal.set_title(title)
+    proposals[proposal_id] = proposal
+    return JSONResponse(status_code=200, content={"message": "Título guardado con éxito."})
+
+
+@router.post('/download-proposal/')
+async def download_proposal(request: DownloadProposalRequest):
+    """ Descarga una propuesta a partir de su ID. """
+    proposal_id = request.proposal_id
+    proposal = proposals[proposal_id]
+    title = proposal.get_title() if proposal.get_title() else proposal_id
+    content = proposal.get_content()
+    doc = Document()
+    # for _, text in content.items():
+    #     html = markdown2.markdown(text)
+    #     temp_html_path = os.path.join(TEMP_DIR, 'temp.html')
+    #     with open(temp_html_path, 'w', encoding='utf-8') as temp_html_file:
+    #         temp_html_file.write(html)
+    #     temp_docx_path = os.path.join(TEMP_DIR, 'temp.docx')
+    #     pypandoc.convert_file(
+    #         temp_html_path, 'docx', format='html', outputfile=temp_docx_path)
+    #     docx_content = Document(temp_docx_path)
+    #     for paragraph in docx_content.paragraphs:
+    #         doc.add_paragraph(paragraph.text)
+    for _, text in content.items():
+        html = markdown2.markdown(text)
+        soup = BeautifulSoup(html, 'html.parser')
+        for element in soup:
+            if element.name == 'p':
+                paragraph = doc.add_paragraph()
+                for part in element:
+                    if part.name == 'strong':
+                        run = paragraph.add_run(part.text)
+                        run.bold = True
+                    else:
+                        paragraph.add_run(part.text)
+    path = os.path.join(TEMP_DIR, f'{title}.docx')
+    doc.save(path)
+    return FileResponse(path, filename=f'{title}.docx',
+                        media_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
 
 
 @router.post("/upload-tdr-streaming/")
@@ -384,4 +485,4 @@ async def upload_tdr_streaming(file: UploadFile = File(...)):
 
     return StreamingResponse(generate(), media_type="application/json")
 
-# TODO - Ofrecer un outline con la estructura de la propuesta donde el usuario puede escoger, modificar y reordenar.
+# TODO - Implementar logica para proposal_id generalizado
